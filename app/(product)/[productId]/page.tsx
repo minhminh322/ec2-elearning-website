1979608f5e9383a1c7187419953d0623aec02cbf
Minh Hodler <ptminh2013@gmail.com>
2023-10-24 11:18:28 -0700

    add UI for product detail page

--- app/(product)/[productId]/page.tsx
+++ app/(product)/[productId]/page.tsx
@@ -34,11 +34,11 @@ const ProductPage = async ({
   const image_base_url = process.env.CDN_URL + "/images/";
   return (
     <div
-      className="grid grid-cols-2 gap-4"
+      className="grid grid-cols-2 gap-4 mt-10"
       style={{ gridTemplateColumns: " 4fr 2fr" }}
     >
       <div className="col-span-1">
-        <div className="flex flex-col max-w-xl mx-auto py-20 space-y-20">
+        <div className="flex flex-col  bg-gray-50 dark:bg-gray-900 shadow-xl shadow-gray-500 dark:shadow-indigo-500  rounded-lg max-w-2xl ml-10 p-7 space-y-10">
           <h1>{product?.name}</h1>
 
           {courses && courses?.length > 0 ? (
@@ -48,8 +48,8 @@ const ProductPage = async ({
           )}
         </div>
       </div>
-      <div className="col-span-1">
-        <div className="flex flex-col items-center justify-center max-w-xl mx-10 my-20 space-y-10">
+      <div className="col-span-1 ">
+        <div className="flex flex-col items-center bg-gray-50 dark:bg-gray-900 shadow-xl shadow-gray-500 dark:shadow-indigo-500 rounded-lg p-7 mr-10 max-w-xl  space-y-5">
           <div className="relative w-full aspect-video rounded-md overflow-hidden">
             <Image
               fill
@@ -64,7 +64,9 @@ const ProductPage = async ({
               courses && courses.length > 0 ? courses[0].id : ""
             }`}
           >
-            <Button>Start Course</Button>
+            <Button className="transition ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-500">
+              Start Course
+            </Button>
           </Link>
         </div>
       </div>
