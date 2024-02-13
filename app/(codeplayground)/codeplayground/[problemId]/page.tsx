b23067c4950c9c7d52289d98b7d106929c9f7b56
Minh Hodler <ptminh2013@gmail.com>
2024-02-12 17:22:07 -0800

    fix production url for submissions api

--- app/(codeplayground)/codeplayground/[problemId]/page.tsx
+++ app/(codeplayground)/codeplayground/[problemId]/page.tsx
@@ -22,6 +22,14 @@ export interface TestCase {
   }
 }[]
 
+async function getData(problemId: string) {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_AWS_BACKEND_BASE_URL}/code-playground?problemId=${problemId}`)
+  if (response.ok) {
+    return response.json()
+  }
+  return null
+}
+
 const CodePlaygroundPage = async ({
   params,
 }: {
@@ -34,15 +42,7 @@ const CodePlaygroundPage = async ({
     return redirect("/");
   }
 
-  const response = await axios.get(
-    `${process.env.NEXT_PUBLIC_AWS_BACKEND_BASE_URL}/code-playground?problemId=${params.problemId}`
-  );
-
-  if (!response.data) {
-    throw new Error("Practice not found");
-  }
-
-  const { content, sourceCode, solution, testCases } = response.data;
+  const { content, sourceCode, solution, testCases } = await getData(params.problemId);
 
   const codePlaygroundProps: CodePlaygroundProps = { userId, problemId: params.problemId, content, sc: sourceCode, solution, testCases };
   return (
